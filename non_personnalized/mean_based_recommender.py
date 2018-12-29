64bc9027df7fbc1400bf53bc90ad4631f9fae570
Salwa Maatoug <maatougsalwa@gmail.com>
2018-12-29 21:37:15 +0100

    update docstring

--- non_personnalized/mean_based_recommender.py
+++ non_personnalized/mean_based_recommender.py
@@ -3,10 +3,19 @@ The mean_based_recommender will provide non personnalized
 recommendations based on the items mean ratings
 """
 class ItemMeanModelProvider:
-    """ItemMeanModel Provider class provides the top N highest rated items
+    """
+    ItemMeanModel Provider class provides the top N highest rated items
     First, it sets the number of top rated items.
     The number of top rated items can be entered by the user and should be a positive integer.
+    
+    Raises:
+        TypeError -- non valid type, should be a positive integer
+        ValueError -- invalid number of top rated items, should be a positive integer
+    
+    Returns:
+        integer -- TBD
     """
+
     def __init__(self, num_top_rated):
         self.num_top_rated = num_top_rated  
     @property
